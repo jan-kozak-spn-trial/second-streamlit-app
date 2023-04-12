d3491bcce0255dd302b73d7b6d0cd8ce14205320
jan-kozak-spn-trial <130061932+jan-kozak-spn-trial@users.noreply.github.com>
2023-04-12 13:23:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@ import streamlit
 
 streamlit.title("Snowflake Trial - Zena's Athleisure Catalog")
 streamlit.text("Pick a sweatsuit color or type")
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
